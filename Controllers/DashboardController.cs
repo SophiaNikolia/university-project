020e410def078d3408b48d6ad3644673f417eb48
SophiaNikolia <sofia.nikolia@gmail.com>
2023-01-05 17:54:44 +0200

    Created the dashboard controller

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace university_project.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Student()
+        {
+            return View();
+        }
+    }
+}
