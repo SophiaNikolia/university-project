97c449a639ca7b5169fcb1a166526df4308a82a2
0x-2FA <2fa_gh@protonmail.com>
2023-01-08 11:40:23 +0200

    Changed the connection string to fetch it from the appsettings json file

--- Models/universityContext.cs
+++ Models/universityContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
 
 namespace university_project.Models
 {
@@ -27,8 +28,12 @@ namespace university_project.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlite("Data Source=university.db");
+                string connectionString = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json")
+                    .Build()
+                    .GetSection("ConnectionStrings")["universityContextConnection"];
+
+                optionsBuilder.UseSqlite(connectionString);
             }
         }
 
