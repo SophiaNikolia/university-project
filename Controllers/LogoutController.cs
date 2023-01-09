33c10d4f62ab396827fbfff26630d295cb6b46b6
0x-2FA <2fa_gh@protonmail.com>
2023-01-09 18:38:22 +0200

    Created a Logout Controller

---
+++ Controllers/LogoutController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using university_project.Areas.Identity.Data;
+
+namespace university_project.Controllers
+{
+    public class LogoutController : Controller
+    {
+        private readonly SignInManager<EntityUser> _signInManager;
+
+        public LogoutController(SignInManager<EntityUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> OnPost()
+        {
+            await _signInManager.SignOutAsync();
+
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
