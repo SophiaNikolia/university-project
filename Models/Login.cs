73281c926e89e97b48fabb9ff82f57555f72a495
SophiaNikolia <sofia.nikolia@gmail.com>
2023-01-11 18:44:32 +0200

    Created login model and connected it to login page

---
+++ Models/Login.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace university_project.Models
+{
+    public class Login
+    {
+        [Required(ErrorMessage = "Please enter your email address!")]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+
+        [Required(ErrorMessage = "Please enter your password!")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+    }
+}
