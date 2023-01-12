6fe1f19391ae99b8ced0c198177f2ce6fe879604
0x-2FA <2fa_gh@protonmail.com>
2023-01-12 16:34:08 +0200

    Changed to lower case

--- Program.cs
+++ Program.cs
@@ -48,6 +48,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=home}/{action=index}/{id?}");
 
 app.Run();
