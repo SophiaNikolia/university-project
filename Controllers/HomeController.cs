c9d9c89f74f2cc8bea87040683e07baa5d8f062e
0x-2FA <2fa_gh@protonmail.com>
2023-01-19 18:11:35 +0200

    Added a check for user's role and redirect him if he is signed in

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,20 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using university_project.Areas.Identity.Data;
 using university_project.Models;
 
 namespace university_project.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly SignInManager<EntityUser> _signInManager;
+        private readonly UserManager<EntityUser> _userManager;
+
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(SignInManager<EntityUser> signInManager, UserManager<EntityUser> userManager, ILogger<HomeController> logger)
         {
+            _signInManager = signInManager;
+            _userManager = userManager;
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            if (_signInManager.IsSignedIn(User) && !User.IsInRole("Admin"))
+            {
+                
+                var user = await _userManager.GetUserAsync(User);
+
+                var role = await _userManager.GetRolesAsync(user);
+                
+                // redirect to Action based on user's role
+                return RedirectToAction(role[0], "Dashboard");
+
+            }
             return RedirectToAction("Index", "Login");
         }
 
