9eeaa662f172e0448650efc9de9363f12d65d9ef
0x-2FA <2fa_gh@protonmail.com>
2023-01-26 19:39:03 +0200

    Created new View Model for changing password

---
+++ ViewModels/ChangePasswordData.cs
@@ -0,0 +1,23 @@
+
+using System.ComponentModel.DataAnnotations;
+
+namespace university_project.ViewModels
+{
+    public partial class ChangePasswordData
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        public string OldPassword { get; set; }
+
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm new password")]
+        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
