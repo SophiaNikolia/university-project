640f26efb2ef7e9c9f9d432d622ce54a0fa7614b
SophiaNikolia <sofia.nikolia@gmail.com>
2023-01-27 22:57:53 +0200

    Created the View Model for the Dashboard

---
+++ ViewModels/DashboardStudentData.cs
@@ -0,0 +1,14 @@
+﻿namespace university_project.ViewModels
+{
+    public class DashboardStudentData
+    {
+        public string StudentName { get; set; }
+        public string StudentSurname { get; set; }
+        public string ProfessorName { get; set; }
+        public string ProfessorSurname { get; set; }
+        public string CourseTitle { get; set; }
+        public decimal? Gpa { get; set; }
+        public int? CompletedCourses { get; set; }
+        public int? Ects { get; set; }
+    }
+}
