115a680131f8bf693658c435255d7f43c1e45e3f
SophiaNikolia <sofia.nikolia@gmail.com>
2023-01-27 23:02:37 +0200

    Created View Model for the Grade view of the student

---
+++ ViewModels/GradesData.cs
@@ -0,0 +1,13 @@
+﻿namespace university_project.ViewModels
+{
+    public class GradesData
+    {
+        public string CourseTitle { get; set; }
+
+        public string Semester { get; set; }
+
+        public long? CourseGrade { get; set; }
+
+        public double SemesterGrade { get; set; }
+    }
+}
