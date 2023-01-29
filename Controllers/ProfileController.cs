9232aceece98797e7f6d4051a8d32ba110e0331a
SophiaNikolia <sofia.nikolia@gmail.com>
2023-01-30 01:23:16 +0200

    Created the html profile page for all the users, styled it with css files and connected them with the View Model page.

--- Controllers/ProfileController.cs
+++ Controllers/ProfileController.cs
@@ -1,13 +1,75 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using university_project.Areas.Identity.Data;
+using university_project.Models;
+using university_project.ViewModels;
 
 namespace university_project.Controllers
 {
     // TODO: Add authorization
     public class ProfileController : Controller
     {
-        public IActionResult Index()
+        private readonly SignInManager<EntityUser> _signInManager;
+        private readonly universityContext _context;
+
+        public ProfileController(SignInManager<EntityUser> signInManager, universityContext context)
         {
-            return View();
+            _signInManager = signInManager;
+            _context = context;
+        }
+
+        public async Task<IActionResult> Student()
+        {
+            var identityUser = await _signInManager.UserManager.GetUserAsync(User);
+            
+            var student = _context.Students.Where(e => e.UsersUsername.Equals(identityUser.UserName)).FirstOrDefault();
+
+            ProfileData profileData = new ProfileData();
+
+            profileData.Name = student.Name;
+            profileData.Surname = student.Surname;
+            profileData.Department = student.Department;
+            profileData.Email = student.UsersUsername + "@unipi.gr";
+            profileData.Role = "Student";
+
+
+            return View(profileData);
+        }
+
+        public async Task<IActionResult> Professor()
+        {
+            var identityUser = await _signInManager.UserManager.GetUserAsync(User);
+
+            var professor = _context.Professors
+                .Where(e => e.UsersUsername.Equals(identityUser.UserName))
+                .FirstOrDefault();
+
+            ProfileData profileData = new ProfileData();
+
+            profileData.Name = professor.Name;
+            profileData.Surname = professor.Surname;
+            profileData.Department = professor.Department;
+            profileData.Email = professor.UsersUsername + "@unipi.gr";
+            profileData.Role = "Professor";
+
+            return View(profileData);
+        }
+
+        public async Task<IActionResult> Secretary()
+        {
+            var identityUser = await _signInManager.UserManager.GetUserAsync(User);
+
+            var secretary = _context.Secretaties.Where(e => e.UsersUsername.Equals(identityUser.UserName)).FirstOrDefault();
+            
+            ProfileData profileData = new ProfileData();
+
+            profileData.Name = secretary.Name;
+            profileData.Surname = secretary.Surname;
+            profileData.Department = secretary.Department;
+            profileData.Email = secretary.UsersUsername + "@sec.unipi.gr";
+            profileData.Role = "Secretary";
+
+            return View(profileData); ;
         }
     }
 }
---
+++ ViewModels/ProfileData.cs
@@ -0,0 +1,16 @@
+﻿namespace university_project.ViewModels
+{
+    public class ProfileData
+    {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Email { get; set; }
+
+        public string Role { get; set; }
+
+        public string Department { get; set; }
+
+    }
+}
